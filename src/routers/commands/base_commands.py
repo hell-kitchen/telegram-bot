8061ee4e1c0ce065d9db14be2988db605189a869
arikash117 <ari.kash117@gmail.com>
2024-04-28 15:37:54 +0300

    added command /help and reply keyboard changed command /help

--- src/routers/commands/base_commands.py
+++ src/routers/commands/base_commands.py
@@ -2,15 +2,35 @@ from aiogram import Router, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 rt = Router(name=__name__)
 
 
+@rt.message(Command('start'))
+async def handler_start(message: types.Message):
+    kb = [
+        [KeyboardButton(text="/start")],
+        [KeyboardButton(text="/help")],
+        [KeyboardButton(text="/get_all")],
+        [KeyboardButton(text="/search")],
+        [KeyboardButton(text="/cancel")],
+
+    ]
+    rkb = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await message.reply(text="Добро пожаловать в HellChickenBot!")
+
+
 @rt.message(Command('help'))
 async def handler_help(message: types.Message):
-    help_text = 'пока беспомощный ботик'
-    await message.answer(text=help_text)
+    await message.reply(text=f"Вот список команд:"
+                             f"\n/start - запуск бота"
+                             f"\n/help - вызов справки"
+                             f"\n/cancel - отменить текущее действие"
+                             f"\n/search - поиск ингредиентов по названию"
+                             f"\n/get_all - вывод всех имеющихся ингредиентов"
+                        )
 
 
 class Form(StatesGroup):
